339c57005ec193326cb75e9be14399224b451a38
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-05-13 13:43:16 +0200

    fixing login page
    on login page after submit return a blanck page and live server it say "the page need a body" that is because is the same path of the lesson "/auth/login" but I have the login page on the enter page or MAINPAGE so I want try if is possible to change the path

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -3,5 +3,12 @@
 // console.log(constants.API_SOCIAL_URL);
 
 import { setRegisterFormListener } from "./handlers/register.mjs";
+import { setLoginFormListener } from "./handlers/login.mjs";
 
-setRegisterFormListener();
+const path = location.pathname;
+
+if (path === "/profile/login/") {
+  setLoginFormListener();
+} else if (path === "/profile/register/") {
+  setRegisterFormListener();
+}
