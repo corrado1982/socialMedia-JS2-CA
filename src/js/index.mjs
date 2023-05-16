b0caf4e28826455ca6cbfd0c14f2d69d439742ee
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-05-16 17:32:21 +0200

    tested some methods

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -4,6 +4,9 @@
 
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
+import * as post from "./api/posts/index.mjs";
+
+// import * as posts from "./api/posts/index.mjs";
 
 const path = location.pathname;
 
@@ -12,3 +15,11 @@ if (path === "/profile/login/") {
 } else if (path === "/profile/register/") {
   setRegisterFormListener();
 }
+
+// post.createPost();
+// post.updatePost();
+// post.removePost();
+// post.getPost();
+// post.getPosts().then(console.log);
+
+post.getPost(6066).then(console.log);
