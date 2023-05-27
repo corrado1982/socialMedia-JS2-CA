77fad2a9e7f8beec2012cd7ecde832e9c49ab6e4
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-05-27 17:09:18 +0200

    take away search form and sent to filter.js

--- src/js/templates/posts.mjs
+++ src/js/templates/posts.mjs
@@ -5,7 +5,7 @@ function postTemplateB(postData) {
   const posts = document.createElement("div");
   posts.classList.add("posts");
 
-  posts.innerHTML += `<div class="card">
+  posts.innerHTML = `<div class="card">
   <a href="/post/index.html?id=${postData.id}" class="post-title" id=${postData.id}>${postData.title} </a> 
   <p class="post-body">${postData.body}</p>
   <div id="postsId" value="${postData.id}">ID: ${postData.id}</div>
@@ -34,49 +34,3 @@ async function testTemplates() {
 }
 
 testTemplates();
-//                SEARCH FORM
-//             ---------------------------
-
-const searchBar = document.getElementById("searchBar");
-const postCards = document.getElementsByClassName("posts");
-
-// function to perform the search
-const searchPosts = () => {
-  const searchTerm = searchBar.value.toLowerCase();
-
-  // loop through all post cards
-  Array.from(postCards).forEach((card) => {
-    const postTitle = card
-      .querySelector(".post-title")
-      .textContent.toLowerCase();
-    const postBody = card.querySelector(".post-body").textContent.toLowerCase();
-
-    // checks if search matches
-    if (postTitle.includes(searchTerm) || postBody.includes(searchTerm)) {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
-    }
-  });
-};
-// add event listener for search input
-searchBar.addEventListener("input", searchPosts);
-//                   SEARCH BY ID FORM
-
-const searchIdForm = document.getElementById("searchBarId");
-
-const searchId = () => {
-  const idInput = searchIdForm.value;
-
-  console.log(idInput);
-  Array.from(postCards).forEach((card) => {
-    const postId = card.querySelector("#postsId").textContent.toLowerCase();
-
-    if (postId.includes(idInput)) {
-      card.style.display = "block";
-    } else {
-      card.style.display = "none";
-    }
-  });
-};
-searchIdForm.addEventListener("input", searchId);
