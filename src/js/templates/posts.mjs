aaf6f642f5aa55afb50f2dae3de155325f883482
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-05-27 14:48:11 +0200

    added search and search by ID

--- src/js/templates/posts.mjs
+++ src/js/templates/posts.mjs
@@ -5,8 +5,14 @@ function postTemplateB(postData) {
   const posts = document.createElement("div");
   posts.classList.add("posts");
 
-  posts.innerHTML = `<a href="/post/index.html?id=${postData.id}" class="post" id=${postData.id}>${postData.title} </a> <p>created ${postData.created}</p>`;
-
+  posts.innerHTML += `<div class="card">
+  <a href="/post/index.html?id=${postData.id}" class="post-title" id=${postData.id}>${postData.title} </a> 
+  <p>${postData.body}</p>
+  <div id="postsId" value="${postData.id}">ID: ${postData.id}</div>
+ 
+  <p>created: ${postData.created}</p>
+  </div>`;
+  //<img class="image-size" src=${postData.media} alt="Image of ${postData.title}" ></img>
   if (postData.media) {
     const img = document.createElement("img");
     img.classList.add("image-size");
@@ -28,3 +34,49 @@ async function testTemplates() {
 }
 
 testTemplates();
+//                SEARCH FORM
+//             ---------------------------
+
+const searchBar = document.getElementById("searchBar");
+const postCards = document.getElementsByClassName("posts");
+
+// function to perform the search
+const searchPosts = () => {
+  const searchTerm = searchBar.value.toLowerCase();
+
+  // loop through all post cards
+  Array.from(postCards).forEach((card) => {
+    const postTitle = card
+      .querySelector(".post-title")
+      .textContent.toLowerCase();
+    // const postBody = card.querySelector(".post-body").textContent.toLowerCase();
+
+    // checks if search matches
+    if (postTitle.includes(searchTerm)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+};
+// add event listener for search input
+searchBar.addEventListener("input", searchPosts);
+//                   SEARCH BY ID FORM
+
+const searchIdForm = document.getElementById("searchBarId");
+
+const searchId = () => {
+  const idInput = searchIdForm.value;
+
+  console.log(idInput);
+  Array.from(postCards).forEach((card) => {
+    const postId = card.querySelector("#postsId").textContent.toLowerCase();
+
+    if (postId.includes(idInput)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+};
+searchIdForm.addEventListener("input", searchId);
